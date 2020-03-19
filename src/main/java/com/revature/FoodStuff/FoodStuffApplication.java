baf4914ac1433e3d44888d941f035b7f39143c33
viccarogg <viccarogg@gmail.com>
2020-03-18 20:46:14 -0400

    Initialized Spring project

---
+++ src/main/java/com/revature/FoodStuff/FoodStuffApplication.java
@@ -0,0 +1,13 @@
+package com.revature.FoodStuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodStuffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/FoodStuff/FoodStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.FoodStuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
