da72eddcdfd476263d0cae2b8f26f083fc6235c7
RaiBear08 <61160598+RaiBear08@users.noreply.github.com>
2020-03-21 07:08:34 -0400

    Ryan Testing Push followers and saved model

---
+++ FoodStuff-master/src/main/java/com/revature/foodstuff/model/Followers.java
@@ -0,0 +1,54 @@
+package com.revature.foodstuff.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "Affiliation")
+public class Followers {
+	 private long affiliationID;
+	 private int followerID;
+	 private int followeeID;
+
+	  @Id
+	  @GeneratedValue(strategy = GenerationType.AUTO)
+	public long getAffiliationID() {
+		return affiliationID;
+	}
+
+	  
+	public void setAffiliationID(long affiliationID) {
+		this.affiliationID = affiliationID;
+	}
+	
+	
+	  @Column(name = "follower_ID", nullable = false)
+	public int getFollowerID() {
+		return followerID;
+	}
+
+	  
+	public void setFollowerID(int followerID) {
+		this.followerID = followerID;
+	}
+	
+	
+	  @Column(name = "followee_ID", nullable = false)
+	public int getFolloweeID() {
+		return followeeID;
+	}
+
+	public void setFolloweeID(int followeeID) {
+		this.followeeID = followeeID;
+	} 
+	  
+	  
+	  
+	  
+}
---
+++ FoodStuff-master/src/main/java/com/revature/foodstuff/model/Saved.java
@@ -0,0 +1,52 @@
+package com.revature.foodstuff.model;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "Saved")
+public class Saved {
+	 private long savedID;
+	 private int postID;
+	 private int userID;
+	
+	 
+	  @Id
+	  @GeneratedValue(strategy = GenerationType.AUTO)
+	 public long getSavedID() {
+		return savedID;
+	}
+	  
+	  
+	public void setSavedID(long savedID) {
+		this.savedID = savedID;
+	}
+	
+	@Column(name = "post_ID", nullable = false)
+	public int getPostID() {
+		return postID;
+	}
+	public void setPostID(int postID) {
+		this.postID = postID;
+	}
+	
+	@Column(name = " user_ID", nullable = false)
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	 
+	 
+	 
+	 
+}
