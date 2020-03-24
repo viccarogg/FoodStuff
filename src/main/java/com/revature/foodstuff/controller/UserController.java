656ee56dfd0d534ba3762bad99b10ee2bc3cab15
Vikki <vswalker96@users.noreply.github.com>
2020-03-23 23:59:14 -0400

    created "createUser" method in UserController. Also noticed through post man tests that none of the @PostMapping for creating work because of constraints.

--- src/main/java/com/revature/foodstuff/controller/UserController.java
+++ src/main/java/com/revature/foodstuff/controller/UserController.java
@@ -12,9 +12,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.foodstuff.exception.ResourceNotFoundException;
@@ -41,6 +43,11 @@ public class UserController {
 		User user = userRepository.findById(userId)
 				.orElseThrow(() -> new ResourceNotFoundException("User not found with this id: " + userId));
 		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping("/users")
+	public User createUser(@RequestBody User user) {
+		return userRepository.save(user);
 	}
 	
 	@PutMapping("/users/{id}")
